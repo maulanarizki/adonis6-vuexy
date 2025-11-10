31b9ebdb96816961cbcfff552341aec1dd27459c
Maulana Rizki <maulana03myr19@gmail.com>
2025-11-10 08:08:35 +0700

    gas

--- database/migrations/1761374495754_create_create_permissions_table.ts
+++ database/migrations/1761374495754_create_create_permissions_table.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name').notNullable().unique()
-      table.string('description').nullable()
+      table.string('name', 240).notNullable().unique()
+      table.string('description', 240).nullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
